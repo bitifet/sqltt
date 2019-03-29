0493d901ff5dbc817db08e04a73508a49797ab17
Joan Miquel Torres <joanmi@gmail.com>
2019-03-29 18:07:22 +0100

    Got rid of "class" and made private methods really private.

--- index.js
+++ index.js
@@ -40,24 +40,12 @@ function resolveEngine(engName) {//{{{
     return [eng, targettedEngName, engineFlavour, cliArgs];
 };//}}}
 
+const sqltt = (function(){ // Sql Tagged Template Engine
 
+    // Private functions:
+    // ------------------
 
-class sqltt { // Sql Template
-    constructor(sourceTpl, options = {}) {//{{{
-        const me = this;
-        me.options = options;
-        me.loadTemplate(sourceTpl);
-        me.checkTemplate();
-        me.sqlCache = {};
-    };//}}}
-    getSource(engFlav) {//{{{
-        const me = this;
-        const src = Object.assign({}, me.source);
-        if (engFlav && src.altsql && src.altsql[engFlav]) src.sql = src.altsql[engFlav];
-        return src;
-    };//}}}
-    hookApply(engName, arg, rarg) {//{{{
-        const me = this;
+    function hookApply(me, engName, arg, rarg) {//{{{
         const hook = me.hooks[arg];
         if (! hook) return rarg;
         const hookt = typeof hook;
@@ -65,79 +53,9 @@ class sqltt { // Sql Template
         if (hookt == "string") return hook.replace(/%/g, rarg);
         throw new Error ("Invalid hook type: " + hookt);
     };//}}}
-    sql(engName = "default") {//{{{
-        const me = this;
-        if (me.sqlCache[engName] !== undefined) return me.sqlCache[engName];
-        const [eng, targettedEngName, engineFlavour, args] = resolveEngine(engName);
-        const sqlt = me.getSource(engineFlavour).sql;
-        function compiler(parts, ...placeholders) {//{{{
-            const sql = [];
-            const literals = this;
-
-            function interpolate(plh, i, bindings = {}) {//{{{
-                switch (typeof plh) {
-                    case "undefined":
-                        if (i == placeholders.length) return ""; // No placeholder at the very end.
-                    case "string":
-                        return me.hookApply(
-                            targettedEngName
-                            , plh
-                            , literals[plh] === undefined
-                                ? eng.indexer(me.argIdx[plh], plh)
-                                : literals[plh]
-                        );
-                    case "function": // Template source:
-                        return plh.sql(compiler.bind(bindings));
-                    case "object":   // Actual sqltt instance:
-                        if (plh instanceof Array) {
-                            if (typeof plh[0] == "string") {
-                                return me.hookApply(targettedEngName, plh[0], plh[0]);
-                            };
-                            return interpolate(plh[0], i, plh[1]);
-                        };
-
-                        // Subtemplate:
-                        // ------------
-                        if ( // Allow source too:
-                            typeof plh.sql == "function"
-                            && ! plh.getSource // sqltt objects has a sql() function too...
-                        ) {
-                            plh = new me.constructor(plh);
-                        };
-
-                        // Actual sqltt instance:
-                        if ("function" == typeof plh.getSource) {
-                            return plh.getSource(engineFlavour).sql(compiler.bind(bindings));
-                        };
-                        // ------------
-
-                    default:
-                        throw new Error("Wrong placehloder type: " + typeof plh);
-                };
-            };//}}}
-
-            for (let i=0; i<parts.length; i++) {
-                sql.push(parts[i]);
-                sql.push(interpolate(placeholders[i], i));
-            };
-
-            return sql.join("");
-        };//}}}
-        const outSql = eng.wrapper.bind(me)(sqlt(compiler.bind({})), args);
-        me.sqlCache[engName] = outSql;
-        return outSql;
-    };//}}}
-    args(data = {}) {//{{{
-        const me = this;
-        if (data instanceof Array) return data;
-            // Accept regular positional parameters too
-            // (No check is made in this case).
-        return me.argList.map(k=>argParser(data[k]));
-    };//}}}
-    getArguments(engineFlavour) {//{{{
+    function getArguments(me, engineFlavour) {//{{{
         // Recursively retrieve arguments from template respecting
         // specified order (if given).
-        const me = this;
         const sourceTpl = me.getSource(engineFlavour);
 
         function argCompiler(parts, ...placeholders){//{{{
@@ -191,8 +109,7 @@ class sqltt { // Sql Template
             , me.options.check_arguments
         );
     };//}}}
-    loadTemplate(inTpl) {//{{{
-        const me = this;
+    function loadTemplate(me, inTpl) {//{{{
         if (typeof inTpl == "function") { // Backward Compatibility Hook{{{
             // Old Style template transpilation:
             const outTpl = inTpl(()=>{});
@@ -200,16 +117,15 @@ class sqltt { // Sql Template
                 "Wrong old-style template format"
             );
             outTpl.sql = $=>inTpl($).sql;
-            return me.loadTemplate(outTpl);
+            return loadTemplate(me, outTpl);
         };//}}}
         me.source = inTpl;
-        me.argList = me.getArguments();
+        me.argList = getArguments(me);
         me.argIdx = hlp.indexArgs(me.argList);
         me.hooks = me.source.hooks || [];
         return inTpl;
     };//}}}
-    checkTemplate() {//{{{
-        const me = this;
+    function checkTemplate(me) {//{{{
         if (typeof me.source.sql != "function") throw new Error (
             "Wrong template format: sql must be a function."
         );
@@ -219,7 +135,7 @@ class sqltt { // Sql Template
                 if (typeof me.source.altsql[engName] != "function") throw new Error (
                     "Wrong template format: alternative sql for "+engName+" must be a function."
                 );
-                const args = me.getArguments(engName);
+                const args = getArguments(me, engName);
                 if (JSON.stringify(args) !== mainArgs) throw new Error (
                     "Arguments mismatch in alternative sql for "+engName+"."
                 );
@@ -227,7 +143,100 @@ class sqltt { // Sql Template
         };
 
     };//}}}
-    concat(str) {//{{{
+
+
+    // Constructor:
+    // ------------
+
+    function sqltt(sourceTpl, options = {}) {//{{{
+        const me = this;
+        me.options = options;
+        loadTemplate(me, sourceTpl);
+        checkTemplate(me);
+        me.sqlCache = {};
+    };//}}}
+
+
+    // Public methods:
+    // ---------------
+
+    sqltt.prototype.getSource = function getSource(engFlav) {//{{{
+        const me = this;
+        const src = Object.assign({}, me.source);
+        if (engFlav && src.altsql && src.altsql[engFlav]) src.sql = src.altsql[engFlav];
+        return src;
+    };//}}}
+    sqltt.prototype.sql = function sql(engName = "default") {//{{{
+        const me = this;
+        if (me.sqlCache[engName] !== undefined) return me.sqlCache[engName];
+        const [eng, targettedEngName, engineFlavour, args] = resolveEngine(engName);
+        const sqlt = me.getSource(engineFlavour).sql;
+        function compiler(parts, ...placeholders) {//{{{
+            const sql = [];
+            const literals = this;
+
+            function interpolate(plh, i, bindings = {}) {//{{{
+                switch (typeof plh) {
+                    case "undefined":
+                        if (i == placeholders.length) return ""; // No placeholder at the very end.
+                    case "string":
+                        return hookApply(
+                            me
+                            , targettedEngName
+                            , plh
+                            , literals[plh] === undefined
+                                ? eng.indexer(me.argIdx[plh], plh)
+                                : literals[plh]
+                        );
+                    case "function": // Template source:
+                        return plh.sql(compiler.bind(bindings));
+                    case "object":   // Actual sqltt instance:
+                        if (plh instanceof Array) {
+                            if (typeof plh[0] == "string") {
+                                return hookApply(me, targettedEngName, plh[0], plh[0]);
+                            };
+                            return interpolate(plh[0], i, plh[1]);
+                        };
+
+                        // Subtemplate:
+                        // ------------
+                        if ( // Allow source too:
+                            typeof plh.sql == "function"
+                            && ! plh.getSource // sqltt objects has a sql() function too...
+                        ) {
+                            plh = new me.constructor(plh);
+                        };
+
+                        // Actual sqltt instance:
+                        if ("function" == typeof plh.getSource) {
+                            return plh.getSource(engineFlavour).sql(compiler.bind(bindings));
+                        };
+                        // ------------
+
+                    default:
+                        throw new Error("Wrong placehloder type: " + typeof plh);
+                };
+            };//}}}
+
+            for (let i=0; i<parts.length; i++) {
+                sql.push(parts[i]);
+                sql.push(interpolate(placeholders[i], i));
+            };
+
+            return sql.join("");
+        };//}}}
+        const outSql = eng.wrapper.bind(me)(sqlt(compiler.bind({})), args);
+        me.sqlCache[engName] = outSql;
+        return outSql;
+    };//}}}
+    sqltt.prototype.args = function args(data = {}) {//{{{
+        const me = this;
+        if (data instanceof Array) return data;
+            // Accept regular positional parameters too
+            // (No check is made in this case).
+        return me.argList.map(k=>argParser(data[k]));
+    };//}}}
+    sqltt.prototype.concat = function concat(str) {//{{{
         const me = this;
         return Object.assign(
             Object.create(Object.getPrototypeOf(me))
@@ -235,7 +244,7 @@ class sqltt { // Sql Template
             , { sql: (...args) => me.sql(...args) + str }
         );
     };//}}}
-    split(engFlav) {//{{{
+    sqltt.prototype.split = function split(engFlav) {//{{{
         const me = this;
         const src = me.getSource(engFlav);
         const opts = Object.assign({}
@@ -250,7 +259,10 @@ class sqltt { // Sql Template
             ), opts)
         });
     };//}}}
-}
+
+    return sqltt;
+
+})();
 
 module.exports = sqltt;
 
