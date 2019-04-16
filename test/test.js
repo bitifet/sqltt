c62d3c7ad1b0cca9f5a2d61fcac6f4c8ddb839f2
Joan Miquel Torres <joanmi@gmail.com>
2019-04-16 11:26:44 +0200

    More tests...
    ...Up to subtemplates (but nesting subtemplates not yet working).

--- test/test.js
+++ test/test.js
@@ -52,30 +52,30 @@ describe('sqltt class', function() {
 
         });//}}}
 
-        it ('Interpolation methods', function() {//{{{
+        it ('Arguments interpolation methods', function() {//{{{
 
             const q = $=>$`s ${"foo"}, ${$.arg("bar")}, ${$.arg(["baz1", "baz2"])}, ${["baz3", "baz4"]} f`;
             assert.equal(
                 hsql(q)
                 , "s $1, $2, ($3), ($4), $5, $6 f"
-            )
+            );
             assert.equal(
                 hsql(q, "postgresql")
                 , "s $1, $2, ($3), ($4), $5, $6 f"
-            )
+            );
             assert.equal(
                 hsql(q, "oracle")
                 , "s :1, :2, (:3), (:4), :5, :6 f"
-            )
+            );
             assert.deepStrictEqual(
                 hargl(q)
                 ,  ["foo", "bar", "baz1", "baz2", "baz3", "baz4"]
-            )
+            );
             assert.deepStrictEqual(
                 hargs(q, {foo: "fooArg", baz1: "baz1Arg"})
                 ,  ["fooArg", null, "baz1Arg", null, null, null]
                 , "Argument list should be generated in right order"
-            )
+            );
 
 
         });//}}}
@@ -100,12 +100,57 @@ describe('sqltt class', function() {
 
         });//}}}
 
+        it ('Literals interpolation method', function() {//{{{
+
+            const q = $=>$`s ${$.literal("foo")}, ${$.literal(["bar", "baz1"])}, ${[$.literal("baz2"), $.literal("baz3")]} f`;
+            assert.equal(
+                hsql(q)
+                , "s foo, (bar), (baz1), baz2, baz3 f"
+            );
+
+        });//}}}
+
+        it ('Subtemplates ($.include()) interpolation method', function() {//{{{
+            const i1 = {sql: $=>$`s ${"i1a1"}, ${"i1a2"} f`, alias: "subq1"};
+            const i2 = {sql: $=>$`s ${"i2a1"}, ${"i2a2"} f`, alias: "subq2"};
+            const q1 = $=>$`s (${$.include(i1, {i1a2: "'fixed_value'"})}) as subq1 f`;
+            const q2 = $=>$`s ${$.include([i1, i2], {i1a2: "'fixed_value'"})} f`;
+            //const q3 = $=>$`s * f ${$.include(q1)} as supernest`;
+            // TODO: Make nesting to work...
+
+            // q1:
+            assert.equal(
+                hsql(q1)
+                , "s (s $1, 'fixed_value' f) as subq1 f"
+            );
+            // q2:
+            assert.equal(
+                hsql(q2)
+                , "s (s $1, 'fixed_value' f) as subq1, (s $2, $3 f) as subq2 f"
+            );
+            assert.equal(
+                hsql(q2, "oracle")
+                , "s (s :1, 'fixed_value' f) subq1, (s :2, :3 f) subq2 f"
+            );
+            // // q3:
+            // assert.equal(
+            //     hsql(q3)
+            //     , "s (s $1, 'fixed_value' f) as subq1 f"
+            // );
+
+            assert.deepStrictEqual(
+                hargl(q2)
+                ,  ["i1a1", "i2a1", "i2a2"]
+            );
+            assert.deepStrictEqual(
+                hargs(q2, {i1a2: "IwontAppear", i2a2: "second"})
+                ,  [null, null, "second"]
+                , "Argument list should be generated in right order"
+            );
+
+        });//}}}
+
     });
-
-
-
-
-
 
 });
 
