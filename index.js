2ee258bcd478873a955f8d22d2821bbd542b7a09
Joan Miquel Torres <joanmi@gmail.com>
2019-08-28 19:29:29 +0200

    New branch globally using soft binding.
    It was an attempt to fix bug in .data() Template API method self cloning
    I originally thought could be caused by (recursive) function binding.
    
    It seems that wasn't the case. But I would keep the work for future
    review in case similar issues would be found.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 "use strict";
 const D = require("./lib/definitions");
 const _priv_ = D.sym_private;
+const hlp = require("./lib/helpers.js");
 const privateMethods = require("./lib/privateMethods.js");
 const publicMethods = require("./lib/tplAPI.js");
 const staticMethods = require("./lib/staticAPI.js");
@@ -15,7 +16,7 @@ function sqltt(sourceTpl, options = {}) {
     me[_priv_] = {};
     me[_priv_].options = options;
     Object.entries(privateMethods).map( // Kinda private methods
-        ([k,v])=>me[_priv_][k]=v.bind(me)
+        ([k,v])=>me[_priv_][k]=hlp.softBind(v, me)
     );
     me[_priv_].loadTemplate(sourceTpl);
     me[_priv_].checkTemplate();
